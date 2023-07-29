f9af9da7a6526239ce2cd6927d13e7bb3b3cb311
THE NEW <teo11113@gmail.com>
2023-07-29 16:37:43 +0700

    tuan1

--- week14/replace/src/store/LoginSlide.js
+++ week14/replace/src/store/LoginSlide.js
@@ -1,18 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const LoginSlide = createSlice({
-    name: 'loginSlide',
-    initialState : {
-        login: false,
+  name: "loginSlide",
+  initialState: {
+    login: false,
+  },
+  reducers: {
+    loginUpdate: (state, action) => {
+      state.login = action.payload;
     },
-reducers:{
-    loginUpdate:(state,action)=>{
-        state.login=action.payload
-    },
-}
-
-})
-export const { loginUpdate } =
-LoginSlide.actions;
+  },
+});
+export const { loginUpdate } = LoginSlide.actions;
 export default LoginSlide;
-
